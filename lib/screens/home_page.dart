2fd04a691fef694d8394c1ef84ca7d79fed8aaac
Lyu Jason <jason.lyu@supergeo.com.tw>
2020-01-08 14:15:35 +0800

    home page 不顯示返回按鈕

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -46,6 +46,7 @@ class _HomePageState extends State<HomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        automaticallyImplyLeading: false,
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
